2edc4e2a9a4d5a94261703a4e944ea7dd6552417
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-03-09 19:39:07 -0500

    fetch nat park data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,1 +1,25 @@
-const apiKeyNatPark = XeD3ty79NiWNMwuU7DVhQM9fye0L5RspxNkmYSXe
+const apiKeyNatPark = "XeD3ty79NiWNMwuU7DVhQM9fye0L5RspxNkmYSXe";
+
+ 
+
+
+var getNatParkInfo = function() {
+    var natParkUrl = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=" + apiKeyNatPark;
+    fetch(natParkUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                console.log(data);
+            });
+            } else {
+                alert('Error: National Park Not Found');
+            }
+        })
+        .catch(function(error) {
+        alert("Unable to connect to National Park API");
+    });
+};
+
+
+getNatParkInfo();
